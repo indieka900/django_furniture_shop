aef162141759fd56b13bc9a59cde39c885fc3ea7
Joseph <indiekaj@gmail.com>
2023-11-05 21:38:39 +0300

    Working code

--- template_app/views.py
+++ template_app/views.py
@@ -1,59 +1,64 @@
-from django.shortcuts import render,redirect
-from .models import Pages, Team, Company,Pages, Testimonials, Blog, ContactUs, Messages, ImageGrid, Services,Shop
+from django.shortcuts import render, redirect
+from .models import Pages, Team, Company, Pages, Testimonials, Blog, ContactUs, Messages, ImageGrid, Services, Shop, SocialMedia
 
+#common to all pages
+def common_data(pageName):
+    return {
+        'testmonials': Testimonials.objects.all().order_by('?'),
+        'pages': Pages.objects.all(),
+        'media': SocialMedia.objects.all(),
+        'company' : Company.objects.last(),
+        'page': Pages.objects.get(page=pageName),
+        'nav' : pageName.lower()
+    }
 
 def home(request):
-    testmonials = Testimonials.objects.all().order_by('?')
-    page = Pages.objects.get(page='Home')
     products = Shop.objects.all().order_by('?')[:3]
     blog = Blog.objects.all().order_by('?')[:3]
     services = Services.objects.all().order_by('?')[:4]
     context = {
-        'nav':'home',
-        'page':page,
-        'testmonials':testmonials,
-        'products':products,
-        'blogs':blog,
-        'services':services,
+        'products': products,
+        'blogs': blog,
+        'services': services,
+        **common_data('Home'),
     }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html', context)
 
 def about(request):
-    testmonials = Testimonials.objects.all().order_by('?')
     services = Services.objects.all().order_by('?')[:4]
     team = Team.objects.all()
-    page = Pages.objects.get(page='About')
     context = {
-        'nav':'about',
-        'testmonials':testmonials,
-        'services':services,
-        'teams':team,
-        'page':page,
+        'services': services,
+        'teams': team,
+        **common_data('About'),
     }
     return render(request, 'about.html', context)
 
 def contact(request):
-    page = Pages.objects.get(page='Contact')
     contactus = ContactUs.objects.all()
     if request.method == 'POST':
         fname = request.POST.get('fname')
         lname = request.POST.get('lname')
         email = request.POST.get('email')
         message = request.POST.get('message')
-        messages = Messages(first_name = fname, last_name= lname, email=email, message= message)
+        messages = Messages(first_name=fname, last_name=lname, email=email, message=message)
         messages.save()
-        return redirect ('/')
+        return redirect('/')
     context = {
-        'page':page,
-        'nav':'contact',
         'contactus': contactus,
+        **common_data('Contact'),
     }
-    return render(request, 'contact.html',context)
+    return render(request, 'contact.html', context)
+
+def blog(request):
+    
+    blog = Blog.objects.all().order_by('?')
+    context = {'blogs':blog,**common_data('Blog'),}
+    return render(request, 'blog.html', context)
 
 def cart(request):
-    page = Pages.objects.get(page='Cart')
     context = {
-        'page':page,
+        **common_data('Cart'),
         'nav':'cart'
     }
     return render(request, 'cart.html', context)
@@ -61,42 +66,28 @@ def cart(request):
 def services(request):
     products = Shop.objects.all().order_by('?')[:3]
     services = Services.objects.all().order_by('?')
-    testmonials = Testimonials.objects.all().order_by('?')
-    page = Pages.objects.get(page='Services')
     context = {
-        'nav':'services',
         'products':products,
         'services':services,
-        'testmonials':testmonials,
-        'page':page,
+        **common_data('Services'),
+        
     }
     return render(request, 'services.html', context)
 
 def shop(request):
     products = Shop.objects.all().order_by('?')
     products1 = Shop.objects.all().order_by('?')
-    page = Pages.objects.get(page='Shop')
     context = {
-        'page':page,
+        **common_data('Shop'),
         'products':products,
         'products1':products1,
-        'nav':'shop'
     }
     return render(request, 'shop.html', context)
 
-def blog(request):
-    testmonials = Testimonials.objects.all().order_by('?')
-    blog = Blog.objects.all().order_by('?')
-    page = Pages.objects.get(page='Blog')
-    context = {'nav':'blog','blogs':blog,'testmonials':testmonials,'page':page,}
-    return render(request, 'blog.html', context)
 
 def thankyou(request):
-    return render(request, 'thankyou.html',{'nav':'thankyou'})
+    return render(request, 'thankyou.html', {'nav': 'thankyou'})
 
 def checkout(request):
-    context = {'nav':'chechout'}
+    context = {'nav': 'chechout'}
     return render(request, 'checkout.html', context)
-
-
-# Create your views here.
